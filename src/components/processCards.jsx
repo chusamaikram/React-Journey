b144309090cb5b1a8e721db8a043fefc0586aa69
chusamaikram <usamachaudhary0345@gmail.com>
2025-11-26 18:07:42 +0500

    adjusted process cards content's visibility using framer motion property "whileInView"

--- src/components/processCards.jsx
+++ src/components/processCards.jsx
@@ -1,14 +1,31 @@
-
+import { motion } from "framer-motion"
 export default function ProcessCard(props) {
-    return(
+    return (
         <>
             <div className="flex flex-col items-start">
-                <h3 className="text-[19px] font-semibold flex items-center gap-2 font-[Poppins]" >
+                <h3 className=" flex items-center gap-2 font-[Poppins]" >
                     <span className="text-base font-medium text-white ">{props.id}</span>
-                    {props.heading}
-                    </h3>
+                    <motion.span
+                        initial={{ opacity: 0.5 }}
+                        whileInView={{
+                            opacity: 1
+                        }}
+                        transition={{
+                            duration: 1.5
+                        }}
+                        className="text-[19px] font-semibold "
+                    >{props.heading} </motion.span>
+                </h3>
                 <span className="text-lg text-white/60 font-bold font-['Plus_Jakarta_Sans'] " >{props.category}</span>
-                <p className="max-w-[553px] mt-2.5 text-base leading-[24px] text-white font-['Plus_Jakarta_Sans'] text-start ">{props.desc}</p>
+                <motion.p
+                    initial={{ opacity: 0.5 }}
+                    whileInView={{
+                        opacity: 1
+                    }}
+                    transition={{
+                        duration: 1.5
+                    }}
+                    className="max-w-[553px] mt-2.5 text-base leading-[24px] text-white font-['Plus_Jakarta_Sans'] text-start ">{props.desc}</motion.p>
             </div>
 
         </>
--- src/pages/home/process.jsx
+++ src/pages/home/process.jsx
@@ -15,7 +15,7 @@ export default function ProcessSection() {
     const containerRef = useRef(null);
     const { scrollYProgress } = useScroll({
         target: containerRef,
-        offset: ["start 400px", "end end"]
+        offset: ["start 300px", "end 300px"]
     });
 
     const [activeImage, setActiveImage] = useState(process1);
